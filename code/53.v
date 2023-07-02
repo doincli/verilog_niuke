cc7b5b94b5e9c8af22ccf4642891ef0124958343
liboyu <doincli@163.com>
2023-07-03 04:03:32 +0800

    add

---
+++ code/53.v
@@ -0,0 +1,15 @@
+`timescale 1ns/1ns
+
+module RAM_1port(
+    input clk,
+    input rst,
+    input enb,
+    input [6:0]addr,
+    input [3:0]w_data,
+    output wire [3:0]r_data
+);
+//*************code***********//
+
+
+//*************code***********//
+endmodule
